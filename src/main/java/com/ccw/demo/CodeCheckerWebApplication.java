e079d858a6d7577317bdfb8c6a4af73a7993dce9
Lamp <dmvoicestudios@gmail.com>
2020-04-19 11:12:52 +0200

    minor changes for war creation

--- src/main/java/com/ccw/demo/CodeCheckerWebApplication.java
+++ src/main/java/com/ccw/demo/CodeCheckerWebApplication.java
@@ -2,6 +2,7 @@ package com.ccw.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,6 +10,11 @@ public class CodeCheckerWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeCheckerWebApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CodeCheckerWebApplication.class);
 	}
 
 }
