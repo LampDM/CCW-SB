b0cef0fd0eb1a8e538355f0f4a8aafe6aca139d8
Lamp <dmvoicestudios@gmail.com>
2020-04-19 10:25:27 +0200

    cleaned up POM and optimised for war creation

--- src/main/java/com/ccw/demo/CodeCheckerWebApplication.java
+++ src/main/java/com/ccw/demo/CodeCheckerWebApplication.java
@@ -2,9 +2,10 @@ package com.ccw.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CodeCheckerWebApplication {
+public class CodeCheckerWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeCheckerWebApplication.class, args);
